c2c4a161e806c90f730c9a8ad7bbc844760cbb20
jher235 <tim668666@gmail.com>
2024-07-13 23:46:17 +0900

    setting:스프링 부트 세팅 - #1

---
+++ src/main/java/com/hackathonteam1/refreshrator/RefreshratorApplication.java
@@ -0,0 +1,13 @@
+package com.hackathonteam1.refreshrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefreshratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RefreshratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hackathonteam1/refreshrator/RefreshratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathonteam1.refreshrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefreshratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
