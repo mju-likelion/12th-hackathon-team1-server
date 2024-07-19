cb6345f37cbd1309484221a097a7b637c979ca96
juryheed <rabitangl20@naver.com>
2024-07-20 03:43:15 +0900

    feat: AuthenticationConfig작성 - #14

---
+++ src/main/java/com/hackathonteam1/refreshrator/authentication/AuthenticationConfig.java
@@ -0,0 +1,31 @@
+package com.hackathonteam1.refreshrator.authentication;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class AuthenticationConfig implements WebMvcConfigurer {
+
+    private final AuthenticationInterceptor authenticationInterceptor;
+    private final AuthenticatedUserArgumentResolver authenticatedUserArgumentResolver;
+
+    //인터셉터 등록 + 경로 설정
+    @Override
+    public void addInterceptors(final InterceptorRegistry registry) {
+        registry.addInterceptor(authenticationInterceptor)
+                .addPathPatterns("/fridge/**")
+                .excludePathPatterns("/auth/signin", "/auth/login","/auth/logout");
+    }
+
+    //컨트롤러 메서드 파라미터에 인증된 유저가 들어가도록 함
+    @Override
+    public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authenticatedUserArgumentResolver);
+    }
+}
