5f90ddc68029ad5d1839e5d7134df1c6b81e6c8d
juryheed <rabitangl20@naver.com>
2024-07-20 03:52:32 +0900

    feat: UserRepository작성 - #14 UserRepository를 만들고 이메일로 user테이블에서 유저를 찾는 기능을 추가 함

--- src/main/java/com/hackathonteam1/refreshrator/repository/UserRepository.java
+++ src/main/java/com/hackathonteam1/refreshrator/repository/UserRepository.java
@@ -7,5 +7,5 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
-    Optional<User> findByEmail(String email);
+    User findByEmail(String email);
 }
