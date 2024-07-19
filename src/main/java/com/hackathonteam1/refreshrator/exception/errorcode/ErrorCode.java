2741b60169ea987e374642d44c7a5ce1591bb61d
juryheed <rabitangl20@naver.com>
2024-07-20 00:00:29 +0900

    feat: 이메일 중복시 나오는 error code 작성 - #10 회원가입 할때 이메일 중복 검사시 이미 존재하는 이메일이면 발생하는 에러코드 번호 4090 메세지 이미 사용중인 이메일 입니다

--- src/main/java/com/hackathonteam1/refreshrator/exception/errorcode/ErrorCode.java
+++ src/main/java/com/hackathonteam1/refreshrator/exception/errorcode/ErrorCode.java
@@ -12,7 +12,7 @@ public enum ErrorCode {
     PATTERN("4001","형식에 맞지 않습니다."),
     NOT_BLANK("4002", "필수값이 공백입니다."),
     LENGTH("4003", "길이가 유효하지 않습니다."),
-    EMAIL("4004", "이메일 형식이 유효하지 않습니다.");
+    EMAIL("4004", "이메일 형식이 유효하지 않습니다."),
 
     //AuthorizedException
 
@@ -21,6 +21,8 @@ public enum ErrorCode {
     //NotFoundException
 
     //ConflictException
+    DUPLICATED_EMAIL("4090", "이미 사용 중인 이메일입니다.");
+
 
     private final String code;
     private final String message;
