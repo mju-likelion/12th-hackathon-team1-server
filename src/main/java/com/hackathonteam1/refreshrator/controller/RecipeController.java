7ae7d6ffb80756fed88758dba340f57b0a96098d
jher235 <tim668666@gmail.com>
2024-07-20 19:49:37 +0900

    feat: 레시피 컨트롤러 기능 추가 - #20 레시피 등록 기능 HttpStatus OK->CREATE으로 수정, 레시피 상세 조회 추가

--- src/main/java/com/hackathonteam1/refreshrator/controller/RecipeController.java
+++ src/main/java/com/hackathonteam1/refreshrator/controller/RecipeController.java
@@ -3,15 +3,16 @@ package com.hackathonteam1.refreshrator.controller;
 import com.hackathonteam1.refreshrator.annotation.AuthenticatedUser;
 import com.hackathonteam1.refreshrator.dto.ResponseDto;
 import com.hackathonteam1.refreshrator.dto.request.recipe.RegisterRecipeDto;
+import com.hackathonteam1.refreshrator.dto.response.recipe.DetailRecipeDto;
 import com.hackathonteam1.refreshrator.entity.User;
 import com.hackathonteam1.refreshrator.service.RecipeService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,9 +22,15 @@ public class RecipeController {
 
     @PostMapping
     public ResponseEntity<ResponseDto<Void>> register(
-            @RequestBody RegisterRecipeDto registerRecipeDto, @AuthenticatedUser User user){
+            @RequestBody @Valid RegisterRecipeDto registerRecipeDto, @AuthenticatedUser User user){
         recipeService.register(registerRecipeDto, user);
-        return new ResponseEntity<>(ResponseDto.res(HttpStatus.OK,"레시피 등록 성공"),HttpStatus.OK);
+        return new ResponseEntity<>(ResponseDto.res(HttpStatus.CREATED,"레시피 등록 성공"),HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{recipe_id}")
+    public ResponseEntity<ResponseDto<DetailRecipeDto>> getDetail(@PathVariable("recipe_id") UUID recipeId){
+        DetailRecipeDto detailRecipeDto = recipeService.getDetail(recipeId);
+        return new ResponseEntity<>(ResponseDto.res(HttpStatus.OK, "레시피 상세 조회 성공", detailRecipeDto),HttpStatus.OK);
     }
 
 }
