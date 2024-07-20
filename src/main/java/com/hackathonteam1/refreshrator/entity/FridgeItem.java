ab6eb2f333a41316ccabb0fd3db9fc8ff495ab92
juryheed <rabitangl20@naver.com>
2024-07-20 20:51:32 +0900

    feat: FridgeItem어노테이션 수정 - #18 Builder어노테이션을 추가

--- src/main/java/com/hackathonteam1/refreshrator/entity/FridgeItem.java
+++ src/main/java/com/hackathonteam1/refreshrator/entity/FridgeItem.java
@@ -1,10 +1,7 @@
 package com.hackathonteam1.refreshrator.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
 
@@ -13,6 +10,7 @@ import java.time.LocalDate;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class FridgeItem extends BaseEntity{
 
     @ManyToOne(optional = false, fetch = FetchType.LAZY)
