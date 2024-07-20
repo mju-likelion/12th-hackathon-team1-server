5bedd1a789f7bebc1297f79fcdee0bc982a57092
juryheed <rabitangl20@naver.com>
2024-07-21 04:10:42 +0900

    feat: 등록한 재료 정보 수정 로직 작성 - #29 - updateIngredientInFridge 작성 - 재료찾기 메서드화 - 냉장고 찾기 메서드화

--- src/main/java/com/hackathonteam1/refreshrator/service/FridgeService.java
+++ src/main/java/com/hackathonteam1/refreshrator/service/FridgeService.java
@@ -5,6 +5,7 @@ import com.hackathonteam1.refreshrator.entity.Fridge;
 import com.hackathonteam1.refreshrator.entity.FridgeItem;
 import com.hackathonteam1.refreshrator.entity.Ingredient;
 import com.hackathonteam1.refreshrator.entity.User;
+import com.hackathonteam1.refreshrator.exception.ForbiddenException;
 import com.hackathonteam1.refreshrator.exception.NotFoundException;
 import com.hackathonteam1.refreshrator.exception.errorcode.ErrorCode;
 import com.hackathonteam1.refreshrator.repository.FridgeItemRepository;
@@ -12,6 +13,8 @@ import com.hackathonteam1.refreshrator.repository.FridgeRepository;
 import com.hackathonteam1.refreshrator.repository.IngredientRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -24,17 +27,16 @@ public class FridgeService {
     public void addIngredientInFridge(AddFridgeDto addFridgeDto, User user){
 
         //재료 찾기
-        Ingredient ingredient=ingredientRepository.findById(addFridgeDto.getIngredientId())
-                .orElseThrow(()-> new NotFoundException(ErrorCode.INGREDIENT_NOT_FOUND));
+        Ingredient ingredient=findIngredient(addFridgeDto);
 
         //냉장고 찾기
-        Fridge fridge=fridgeRepository.findByUser(user)
-                .orElseThrow(()-> new NotFoundException(ErrorCode.FRIDGE_NOT_FOUND));
+        Fridge fridge=findFridge(user);
 
         //재료 정보 입력
         FridgeItem fridgeItem=FridgeItem.builder()
                 .fridge(fridge)
                 .ingredient(ingredient)
+                .user(user)
                 .expiredDate(addFridgeDto.getExpiredDate())
                 .quantity(addFridgeDto.getQuantity())
                 .storage(defindStorage(addFridgeDto.getStorage()))
@@ -45,11 +47,50 @@ public class FridgeService {
         fridgeItemRepository.save(fridgeItem);
     }
 
+    public void updateIngredientInFridge(UUID id, AddFridgeDto addFridgeDto, User user){
+
+        //수정할 재료 찾기
+        FridgeItem fridgeItem=fridgeItemRepository.findById(id)
+                .orElseThrow(()-> new NotFoundException(ErrorCode.FRIDGE_ITEM_NOT_FOUND));
+
+        //유저가 등록한 재료인지 검사
+        checkAuth(user,fridgeItem.getUser());
+
+        //수정하기
+        fridgeItem.setExpiredDate(addFridgeDto.getExpiredDate());
+        fridgeItem.setQuantity(addFridgeDto.getQuantity());
+        fridgeItem.setStorage(defindStorage(addFridgeDto.getStorage()));
+        fridgeItem.setMemo(addFridgeDto.getMemo());
+
+        //저장하기
+        fridgeItemRepository.save(fridgeItem);
+    }
+
+    //저장방법 결정 메서드
     private FridgeItem.Storage defindStorage(String storage){
         return switch (storage){
             case "상온"-> FridgeItem.Storage.STORE_AT_ROOM_TEMPERATURE;
             case "냉동"-> FridgeItem.Storage.FROZEN;
             default -> FridgeItem.Storage.REFRIGERATED;
         };
+    }
+
+    //데이터베이스에서 재료id로 재료를 찾는 메서드
+    private Ingredient findIngredient(AddFridgeDto addFridgeDto){
+        return ingredientRepository.findById(addFridgeDto.getIngredientId())
+                .orElseThrow(()-> new NotFoundException(ErrorCode.INGREDIENT_NOT_FOUND));
+    }
+
+    //데이터베이스에서 유저의 냉장고를 찾는 메서드
+    private Fridge findFridge(User user){
+        return fridgeRepository.findByUser(user)
+                .orElseThrow(()-> new NotFoundException(ErrorCode.FRIDGE_NOT_FOUND));
+    }
+
+    //권한 확인
+    private void checkAuth(User writer, User user){
+        if(!writer.getId().equals(user.getId())){
+            throw new ForbiddenException(ErrorCode.FRIDGE_ITEM_FORBIDDEN);
+        }
     }
 }
