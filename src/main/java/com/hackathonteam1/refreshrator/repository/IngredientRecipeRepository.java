3147f38a4e7e6ed88a42c75ec48a86ab388a5195
jher235 <tim668666@gmail.com>
2024-07-20 19:48:37 +0900

    feat: IngredientRecipeRepository 메서드 추가 - #20 레시피로 레시피의 재료들을 찾는 메서드 추가.

--- src/main/java/com/hackathonteam1/refreshrator/repository/IngredientRecipeRepository.java
+++ src/main/java/com/hackathonteam1/refreshrator/repository/IngredientRecipeRepository.java
@@ -1,9 +1,13 @@
 package com.hackathonteam1.refreshrator.repository;
 
 import com.hackathonteam1.refreshrator.entity.IngredientRecipe;
+import com.hackathonteam1.refreshrator.entity.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface IngredientRecipeRepository extends JpaRepository<IngredientRecipe, UUID> {
+    Optional<List<IngredientRecipe>> findAllByRecipe(Recipe recipe);
 }
