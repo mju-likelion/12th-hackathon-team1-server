39f6121a7ca7c94e8622f71d6bed67a8681d3109
jher235 <tim668666@gmail.com>
2024-07-21 01:15:41 +0900

    feat: 레시피명, 조리법 수정을 위한 컨트롤러 추가 - #20

--- src/main/java/com/hackathonteam1/refreshrator/controller/RecipeController.java
+++ src/main/java/com/hackathonteam1/refreshrator/controller/RecipeController.java
@@ -2,6 +2,7 @@ package com.hackathonteam1.refreshrator.controller;
 
 import com.hackathonteam1.refreshrator.annotation.AuthenticatedUser;
 import com.hackathonteam1.refreshrator.dto.ResponseDto;
+import com.hackathonteam1.refreshrator.dto.request.recipe.ModifyRecipeDto;
 import com.hackathonteam1.refreshrator.dto.request.recipe.RegisterRecipeDto;
 import com.hackathonteam1.refreshrator.dto.response.recipe.DetailRecipeDto;
 import com.hackathonteam1.refreshrator.entity.User;
@@ -32,5 +33,13 @@ public class RecipeController {
         DetailRecipeDto detailRecipeDto = recipeService.getDetail(recipeId);
         return new ResponseEntity<>(ResponseDto.res(HttpStatus.OK, "레시피 상세 조회 성공", detailRecipeDto),HttpStatus.OK);
     }
+
+    @PatchMapping("/{recipe_id}")
+    public ResponseEntity<ResponseDto<Void>> modify(
+            @RequestBody @Valid ModifyRecipeDto modifyRecipeDto, @AuthenticatedUser User user, @PathVariable("recipe_id") UUID recipeId){
+        recipeService.modifyContent(modifyRecipeDto, user, recipeId);
+        return new ResponseEntity<>(ResponseDto.res(HttpStatus.OK,"레시피 수정 성공"),HttpStatus.OK);
+    }
+
 
 }
