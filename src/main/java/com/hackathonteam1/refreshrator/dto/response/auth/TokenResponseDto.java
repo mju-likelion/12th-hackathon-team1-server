51c1ce40130708f63b15894213b71527d57076d7
jher235 <tim668666@gmail.com>
2024-07-28 03:25:07 +0900

    feat: TokenResponseDto추가 - #92

--- src/main/java/com/hackathonteam1/refreshrator/dto/response/auth/TokenResponseDto.java
+++ src/main/java/com/hackathonteam1/refreshrator/dto/response/auth/TokenResponseDto.java
@@ -1,10 +1,14 @@
 package com.hackathonteam1.refreshrator.dto.response.auth;
 
+import com.hackathonteam1.refreshrator.entity.RefreshToken;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class TokenResponseDto {
     private String AccessToken;
+    private RefreshToken refreshToken;
 }
