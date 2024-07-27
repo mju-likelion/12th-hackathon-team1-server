9a6e0d8bd2f50c1898c11349c5cd79ae301fd1a9
jher235 <tim668666@gmail.com>
2024-07-27 12:27:21 +0900

    feat: Dto에 표현되는 내용 추가 - #90 생성 시간, 수정 시간을 추가하고 detail에는 누락되었던 좋아요 수도 나타나도록 변경

--- src/main/java/com/hackathonteam1/refreshrator/dto/response/recipe/DetailRecipeDto.java
+++ src/main/java/com/hackathonteam1/refreshrator/dto/response/recipe/DetailRecipeDto.java
@@ -8,6 +8,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -20,12 +21,16 @@ public class DetailRecipeDto {
     private String name;
     private List<IngredientRecipeResponseDto> ingredientRecipes;
     private String cookingStep;
+    private int likeCount;
     private ImageDto image;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
     public static DetailRecipeDto mapping(Recipe recipe, List<IngredientRecipe> ingredientRecipes){
         List<IngredientRecipeResponseDto> ingredientDtos = ingredientRecipes.stream().map(
                 i->IngredientRecipeResponseDto.changeToDto(i)).collect(Collectors.toList());
         return new DetailRecipeDto(recipe.getId(), recipe.getName(), ingredientDtos,
-                recipe.getCookingStep(),ImageDto.mapping(recipe.getImage()));
+                recipe.getCookingStep(), recipe.getLikeCount(), ImageDto.mapping(recipe.getImage()),
+                recipe.getCreatedAt(), recipe.getUpdatedAt());
     }
 }
