77585dc17674df5ab703285a2ad6fe13dd70d77d
jher235 <tim668666@gmail.com>
2024-07-30 23:38:28 +0900

    feat: 캐시를 인지시키도록 명시 -#98

--- src/main/java/com/hackathonteam1/refreshrator/RefreshratorApplication.java
+++ src/main/java/com/hackathonteam1/refreshrator/RefreshratorApplication.java
@@ -2,10 +2,12 @@ package com.hackathonteam1.refreshrator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableCaching
 public class RefreshratorApplication {
 
     public static void main(String[] args) {
